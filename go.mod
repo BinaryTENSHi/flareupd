743b28a6a366ace0e03b4ee8cbc900729359952a
BinaryTENSHi <binarytenshi@gmail.com>
2020-02-19 09:17:58 +0100

    Use cloudflare-go, IPv6 support

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/BinaryTENSHi/flareupd
 
 go 1.13
+
+require github.com/cloudflare/cloudflare-go v0.11.2
--- main.go
+++ main.go
@@ -1,11 +1,9 @@
 package main
 
 import (
-	"bytes"
-	"encoding/json"
-	"fmt"
 	"io/ioutil"
 	"log"
+	"net"
 	"net/http"
 	"os"
 	"os/signal"
@@ -13,51 +11,35 @@ import (
 	"strings"
 	"syscall"
 	"time"
+
+	"github.com/cloudflare/cloudflare-go"
 )
 
 const (
-	CloudflareUrl = "https://api.cloudflare.com/client/v4"
-	IpInfoUrl     = "https://ipinfo.io/ip"
+	EnvVarIp4InfoUrl = "IP4_INFO_URL"
+	Ip4InfoUrl       = "https://v4.ident.me/"
+	EnvVarIp6InfoUrl = "IP6_INFO_URL"
+	Ip6InfoUrl       = "https://v6.ident.me/"
 
-	EnvVarCfApiKey = "CF_API_KEY"
-	EnvVarCfEmail  = "CF_API_EMAIL"
-	EnvVarCfZoneId = "CF_ZONE_ID"
+	EnvVarCfApiKey   = "CF_API_KEY"
+	EnvVarCfEmail    = "CF_API_EMAIL"
+	EnvVarCfZoneName = "CF_ZONE_NAME"
 
 	EnvVarRefresh = "REFRESH"
-	EnvVarRecord  = "RECORD"
+	EnvVarEntry   = "ENTRY"
 )
 
-type vars struct {
-	ApiKey  string
-	Email   string
-	ZoneId  string
-	Refresh time.Duration
-	Record  string
-}
-
-type record struct {
-	Id      string `json:"id,omitempty"`
-	Type    string `json:"type"`
-	Name    string `json:"name"`
-	Content string `json:"content"`
-}
-
-type records struct {
-	Success bool     `json:"success"`
-	Result  []record `json:"result"`
-}
-
-func ensureEnvVariable(variable string) string {
-	value := strings.TrimSpace(os.Getenv(variable))
-	if len(value) == 0 {
-		log.Fatalf("Required environment variable '%s' is not set!", variable)
+func requiredEnvVariable(variable string) string {
+	if val, ok := os.LookupEnv(variable); ok {
+		return val
 	}
 
-	return value
+	log.Fatalf("Required environment variable '%s' is not set!", variable)
+	return ""
 }
 
-func ensureEnvVariableTime(variable string) time.Duration {
-	value := ensureEnvVariable(variable)
+func requiredEnvVariableTime(variable string) time.Duration {
+	value := requiredEnvVariable(variable)
 	t, err := strconv.Atoi(value)
 	if err != nil {
 		log.Fatalf("Environment variable '%s' is not a number", variable)
@@ -65,128 +47,175 @@ func ensureEnvVariableTime(variable string) time.Duration {
 	return time.Duration(t) * time.Second
 }
 
-func externalIp() (string, error) {
-	resp, err := http.DefaultClient.Get(IpInfoUrl)
+func optionalEnvVariable(variable string, def string) string {
+	if val, ok := os.LookupEnv(variable); ok {
+		return val
+	}
+
+	return def
+}
+
+func main() {
+	apiKey := requiredEnvVariable(EnvVarCfApiKey)
+	email := requiredEnvVariable(EnvVarCfEmail)
+	zoneName := requiredEnvVariable(EnvVarCfZoneName)
+	refresh := requiredEnvVariableTime(EnvVarRefresh)
+	entry := requiredEnvVariable(EnvVarEntry)
+
+	ip4Url := optionalEnvVariable(EnvVarIp4InfoUrl, Ip4InfoUrl)
+	ip6Url := optionalEnvVariable(EnvVarIp6InfoUrl, Ip6InfoUrl)
+
+	api, err := cloudflare.New(apiKey, email)
+	if err != nil {
+		log.Fatalf("Failed to create cloudflare client: %v", err)
+	}
+
+	zoneId, err := api.ZoneIDByName(zoneName)
+	if err != nil {
+		log.Fatalf("Failed to find zone '%s': %v", zoneName, err)
+	}
+
+	log.Println("Starting flareupd...")
+
+	ticker := time.Tick(refresh)
+	cancel := make(chan os.Signal, 1)
+	signal.Notify(cancel, syscall.SIGINT, syscall.SIGTERM, syscall.SIGQUIT)
+
+	ip4Updater := &FlareUpdater{
+		Api:     api,
+		Fetcher: &IpFetcher{Url: ip4Url},
+		Type:    "A",
+		ZoneId:  zoneId,
+		Name:    entry + "." + zoneName,
+	}
+
+	ip6Updater := &FlareUpdater{
+		Api:     api,
+		Fetcher: &IpFetcher{Url: ip6Url},
+		Type:    "AAAA",
+		ZoneId:  zoneId,
+		Name:    entry + "." + zoneName,
+	}
+
+	updaters := make([]*FlareUpdater, 0, 2)
+
+	if ip4Updater.Valid() {
+		updaters = append(updaters, ip4Updater)
+	} else {
+		log.Println("IPv4 updater is not valid: disabled")
+	}
+
+	if ip6Updater.Valid() {
+		updaters = append(updaters, ip6Updater)
+	} else {
+		log.Println("IPv6 updater is not valid: disabled")
+	}
+
+	for {
+		for _, u := range updaters {
+			if err := u.UpdateContent(); err != nil {
+				log.Printf("Failed to update record: %v", err)
+			}
+		}
+
+		select {
+		case <-ticker:
+			continue
+
+		case <-cancel:
+			log.Println("Stopping flareupd...")
+			os.Exit(0)
+		}
+	}
+}
+
+type FlareUpdater struct {
+	Api     *cloudflare.API
+	Fetcher *IpFetcher
+	Type    string
+	ZoneId  string
+	Name    string
+}
+
+func (f *FlareUpdater) Valid() bool {
+	ip, err := f.Fetcher.FetchIp()
+	if err != nil {
+		log.Printf("Failed to fetch an IP from '%s'", f.Fetcher.Url)
+		return false
+	}
+
+	parsed := net.ParseIP(ip)
+	if parsed == nil {
+		log.Printf("Failed to parse response from '%s' to an IP", f.Fetcher.Url)
+		log.Printf(" -- response: %s", ip)
+		return false
+	}
+
+	return true
+}
+
+func (f *FlareUpdater) UpdateContent() error {
+	ip, err := f.Fetcher.FetchIp()
+	if err != nil {
+		return err
+	}
+
+	existing, err := f.Api.DNSRecords(f.ZoneId, cloudflare.DNSRecord{Type: f.Type})
+	if err != nil {
+		return err
+	}
+
+	for _, e := range existing {
+		if e.Name == f.Name {
+			if e.Content == ip {
+				return nil
+			}
+
+			return f.update(e.ID, ip)
+		}
+	}
+
+	return f.create(ip)
+}
+
+func (f *FlareUpdater) update(id string, ip string) error {
+	record := cloudflare.DNSRecord{
+		ID:      id,
+		Type:    f.Type,
+		Name:    f.Name,
+		Content: ip,
+	}
+
+	log.Printf("Updating record (%s) '%s' to IP '%s'", f.Type, f.Name, ip)
+	return f.Api.UpdateDNSRecord(f.ZoneId, id, record)
+}
+
+func (f *FlareUpdater) create(ip string) error {
+	record := cloudflare.DNSRecord{
+		Type:    f.Type,
+		Name:    f.Name,
+		Content: ip,
+	}
+
+	log.Printf("Updating record (%s) '%s' to IP '%s'", f.Type, f.Name, ip)
+	_, err := f.Api.CreateDNSRecord(f.ZoneId, record)
+	return err
+}
+
+type IpFetcher struct {
+	Url string
+}
+
+func (i *IpFetcher) FetchIp() (string, error) {
+	res, err := http.DefaultClient.Get(i.Url)
 	if err != nil {
 		return "", err
 	}
 
-	bs, err := ioutil.ReadAll(resp.Body)
+	bs, err := ioutil.ReadAll(res.Body)
 	if err != nil {
 		return "", err
 	}
 
 	return strings.TrimSpace(string(bs)), nil
-}
-
-func updateDomain(vars vars) error {
-	external, err := externalIp()
-	if err != nil {
-		return err
-	}
-
-	log.Printf("External IP is '%s'. Updating record...", external)
-
-	return updateRecord(vars, external)
-}
-
-func getRecords(vars vars) ([]record, error) {
-	url := fmt.Sprintf("%s/zones/%s/dns_records", CloudflareUrl, vars.ZoneId)
-	req, err := http.NewRequest(http.MethodGet, url, nil)
-	if err != nil {
-		return nil, err
-	}
-
-	req.Header.Add("X-Auth-Email", vars.Email)
-	req.Header.Add("X-Auth-Key", vars.ApiKey)
-
-	resp, err := http.DefaultClient.Do(req)
-	if err != nil {
-		return nil, err
-	}
-
-	records := &records{}
-	if err := json.NewDecoder(resp.Body).Decode(records); err != nil {
-		return nil, err
-	}
-
-	return records.Result, nil
-}
-
-func updateRecord(vars vars, external string) error {
-	records, err := getRecords(vars)
-	if err != nil {
-		return err
-	}
-
-	method := http.MethodPost
-	url := fmt.Sprintf("%s/zones/%s/dns_records", CloudflareUrl, vars.ZoneId)
-	for _, r := range records {
-		if r.Name == vars.Record {
-			log.Printf("Record exists '%s'", r.Id)
-			url = fmt.Sprintf("%s/%s", url, r.Id)
-			method = http.MethodPut
-		}
-	}
-
-	record := record{
-		Type:    "A",
-		Name:    vars.Record,
-		Content: external,
-	}
-
-	bs, err := json.Marshal(record)
-	if err != nil {
-		return err
-	}
-
-	req, err := http.NewRequest(method, url, bytes.NewBuffer(bs))
-	if err != nil {
-		return err
-	}
-
-	req.Header.Add("X-Auth-Email", vars.Email)
-	req.Header.Add("X-Auth-Key", vars.ApiKey)
-
-	resp, err := http.DefaultClient.Do(req)
-	if err != nil {
-		return err
-	}
-
-	log.Println(resp.Status)
-	return nil
-}
-
-func main() {
-	vars := vars{}
-	vars.ApiKey = ensureEnvVariable(EnvVarCfApiKey)
-	vars.Email = ensureEnvVariable(EnvVarCfEmail)
-	vars.ZoneId = ensureEnvVariable(EnvVarCfZoneId)
-	vars.Refresh = ensureEnvVariableTime(EnvVarRefresh)
-	vars.Record = ensureEnvVariable(EnvVarRecord)
-
-	log.Println("Starting flareupd...")
-
-	ticker := time.Tick(vars.Refresh)
-	cancel := make(chan os.Signal)
-	signal.Notify(cancel, syscall.SIGINT, syscall.SIGTERM, syscall.SIGQUIT)
-
-	// Immediately update the record
-	if err := updateDomain(vars); err != nil {
-		log.Printf("Failed to update domain: %v", err)
-	}
-
-	for {
-		select {
-		case <-ticker:
-			log.Println("Updating...")
-			if err := updateDomain(vars); err != nil {
-				log.Printf("Failed to update domain: %v", err)
-			}
-
-		case <-cancel:
-			log.Println("Shutting down")
-			os.Exit(0)
-		}
-	}
 }
